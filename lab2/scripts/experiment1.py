cc6d6a9c69ddb58d41e4b332510a028d44c0c340
Byron Wasti <byron.wasti@gmail.com>
2017-02-03 10:24:30 -0500

    More data!

--- lab2/scripts/experiment1.py
+++ lab2/scripts/experiment1.py
@@ -2,46 +2,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
-import smu
 
-s = smu.smu()
-TAKE_NEW_DATA = False
+TAKE_NEW_DATA = True
 FILENAME = "data/data.csv"
 
-
 if TAKE_NEW_DATA:
+    import smu
+    s = smu.smu()
+    
     f = open(FILENAME, "wb")
     writer = csv.writer(f)
 
-    Iin = np.logspace(-9, -2, 1000)[::-1]
-    #V = np.logspace(
-    vout = []
-    for i in Iin:
-        s.set_current(1, i)
+    #Iin = np.logspace(-9, -2, 100)[::-1]
+    Vin = np.linspace(0.2, 0.8, 100)
+
+    #vout = []
+    iout = []
+    for i in Vin:
+        #s.set_current(1, i)
+        s.set_voltage(1, i)
         s.autorange(1)
-        vout.append(s.get_voltage(1))
+        #vout.append(s.get_voltage(1))
+        iout.append(s.get_current(1))
 
-    s.set_current(1, 0.)
+    s.set_voltage(1, 0.)
 
-    x = Iin
-    y = vout
+    #x = Iin
+    #y = vout
+    x = Vin
+    y = iout
     data = zip(x, y)
     writer.writerows(data)
     f.close()
 
 
 if not TAKE_NEW_DATA:
-    f = open(FILENAME, "rb")
-    reader = csv.reader(f, ',')
-    x = []
-    y = []
-    for row in reader:
-        x.append[row[0]]
-        y.append[row[1]]
+    with open(FILENAME, 'r') as f:
+        reader = csv.reader(f)
+        x = []
+        y = []
+        for row in reader:
+            x.append(row[0])
+            y.append(row[1])
 
 
 if True:
-    plt.semilogx(x, y, '.')
+    plt.semilogy(x, y, '.')
     plt.show()
 
 
