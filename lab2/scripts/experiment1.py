f38eb997827ce702fb05dcb70391f049646a3bfe
Byron Wasti <byron.wasti@gmail.com>
2017-02-03 09:34:20 -0500

    Adding data

--- lab2/scripts/experiment1.py
+++ lab2/scripts/experiment1.py
@@ -5,17 +5,43 @@ import csv
 import smu
 
 s = smu.smu()
-TAKE_NEW_DATA = True
+TAKE_NEW_DATA = False
+FILENAME = "data/data.csv"
 
-f = open("data_experiment1.csv", "wb")
-writer = csv.writer(f)
 
 if TAKE_NEW_DATA:
-    pass
+    f = open(FILENAME, "wb")
+    writer = csv.writer(f)
+
+    Iin = np.logspace(-9, -2, 1000)[::-1]
+    #V = np.logspace(
+    vout = []
+    for i in Iin:
+        s.set_current(1, i)
+        s.autorange(1)
+        vout.append(s.get_voltage(1))
+
+    s.set_current(1, 0.)
+
+    x = Iin
+    y = vout
+    data = zip(x, y)
+    writer.writerows(data)
+    f.close()
 
 
-data = zip(x, y)
-writer.writerows(data)
-f.close()
+if not TAKE_NEW_DATA:
+    f = open(FILENAME, "rb")
+    reader = csv.reader(f, ',')
+    x = []
+    y = []
+    for row in reader:
+        x.append[row[0]]
+        y.append[row[1]]
+
+
+if True:
+    plt.semilogx(x, y, '.')
+    plt.show()
 
 
