a9a8f4563b7e48c71e1f192408ebe686eea6267e
Byron Wasti <byron.wasti@gmail.com>
2017-02-08 20:35:41 -0500

    Fixing plots w/ better text

--- lab2/scripts/plotting2.py
+++ lab2/scripts/plotting2.py
@@ -17,14 +17,26 @@ with open(VOL_CUR, 'r') as f:
 
 r_d = np.diff(Vout)/np.diff(Iin)
 
-U_T = 2.886611e-02
+def func(x, a):
+    return a/x
+
+popt, pcov = curve_fit(func, Iin[:-1], r_d, p0=(1e-2))
+print(popt, pcov)
+U_T = popt[0]
 r_d_theoretical = [ U_T/x for x in Iin[:-1] ]
+
+
+#U_T = 2.886611e-02
+#r_d_theoretical = [ U_T/x for x in Iin[:-1] ]
 
 plt.loglog(Iin[:-1], r_d, '.', label="Experimental")
 plt.loglog(Iin[:-1], r_d_theoretical, '-', label="Theoretical")
+
+
 plt.xlabel("Current (A)", fontsize=20)
 plt.ylabel("Incremental Resistance ($\Omega$)", fontsize=20)
 plt.title("Incremental Resistance vs. Current", fontsize=20)
 plt.legend(fontsize=18)
+plt.text(1e-5, 1e6, "Fit: $r_d = U_T/I$\n$U_T$=%e$\Omega A$" % U_T, fontsize=20)
 #plt.plot(Iin, Vout, '.')
 plt.show()
