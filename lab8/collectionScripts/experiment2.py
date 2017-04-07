90b89147b024db1247fd15199926c21e660c8c62
Byron Wasti <byron.wasti@gmail.com>
2017-04-07 10:08:12 -0400

    Scripts

---
+++ lab8/collectionScripts/experiment2.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import csv
+from time import sleep
+
+TAKE_NEW_DATA = True
+FILENAME = "../data/exp2_data1_diffModeGain_1.csv"
+
+if TAKE_NEW_DATA:
+    import smu
+    s = smu.smu()
+    
+    f = open(FILENAME, "wb")
+    writer = csv.writer(f)
+
+    v_in = np.linspace(-50e-3, 50e-3, 200)
+    v_out = []
+
+    s.set_current(2, 0.)
+    for n in  v_in:
+        s.set_voltage(1, n)
+
+        s.autorange(1)
+        s.autorange(2)
+    
+        v_out.append(s.get_voltage(2))
+
+    s.set_voltage(1, 0.)
+
+    data = zip(v_in, v_out)
+    writer.writerow(["V_input", "V_out"])
+    writer.writerows(data)
+    f.close()
+
+    x = v_in
+    y1 = v_out
+
+
+if not TAKE_NEW_DATA:
+    with open(FILENAME, 'r') as f:
+        reader = csv.reader(f)
+        x = []
+        y1 = []
+
+        for i, row in enumerate(reader):
+            if i == 0 : continue
+            x.append(row[0])
+            y1.append(row[1])
+
+
+if True:
+    fig, ax1 = plt.subplots()
+
+    ax1.plot(x, y1, '.', label="i_channel")
+    ax1.set_xlabel("Voltage In")
+    ax1.set_ylabel("Voltage Out")
+
+    plt.show()
+
+
---
+++ lab8/plottingScripts/diffModeGain.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import csv
+
+
+def getData(FILENAME):
+    x = []
+    y = []
+
+    with open(FILENAME, 'r') as f:
+        reader = csv.reader(f)
+        for i, row in enumerate(reader):
+            if i==0 : continue
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+
+    return np.array(x), np.array(y)
+
+
+def plotStuff( x, y, label):
+    plt.plot(x, y, '.',  label=label)
+
+
+def plot():
+    plt.xlabel("$V_{in}$")
+    plt.ylabel("$V_{out}$")
+    plt.title("Weak VTC")
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    v_in, v_out = getData("../data/exp2_data1_diffModeGain_1.csv")
+
+    plotStuff( v_in, v_out, "Experimental Data" )
+
+    plot()
